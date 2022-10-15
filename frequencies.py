a0bb0443b22e869fed2245c9b4be756bee39496d
Chin Wan <chin.wan@kcl.ac.uk>
2022-10-15 19:31:24 +0100

    first try

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,8 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    item2 = list(map(str, items))
+    frequencies = {x: item2.count(x) for x in item2}
     return frequencies
